a5c887e0c27272db4dd826fedde96ecb46e9bcb0
Maddie Jones <maddyg91@gmail.com>
2019-01-08 21:40:39 -0700

    Fixed bug on path for deleting review all green

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,9 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'merchants#show', as: 'dashboard'
   namespace :dashboard do
-    resources :users, only: [:index] 
+    resources :users, only: [:index]
+    get '/potential', to: 'users#potential'
+
     resources :orders, only: [:show] do
       patch '/items/:id/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
     end
